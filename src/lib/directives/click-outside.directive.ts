daeaec801d7db73130e886b9e77d802685345020
matt@codeveloped.co.uk <matt@codeveloped.co.uk>
2023-04-16 20:45:21 +0100

    Init

---
+++ src/lib/directives/click-outside.directive.ts
@@ -0,0 +1,22 @@
+import {Directive, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+    selector: '[clickOutside]'
+})
+export class ClickOutsideDirective {
+    @Output() clickOutside: EventEmitter<any> = new EventEmitter();
+
+    constructor(
+        private elementRef: ElementRef
+    ) {
+    }
+
+    @HostListener('document:click', ['$event', '$event.target'])
+    public onClick($event, targetElement) {
+        const isClickedInside = this.elementRef.nativeElement.contains(targetElement);
+        if (!isClickedInside) {
+            this.clickOutside.emit($event);
+        }
+    }
+
+}
